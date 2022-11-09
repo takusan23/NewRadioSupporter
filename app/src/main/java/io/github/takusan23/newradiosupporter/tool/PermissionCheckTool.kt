2d640b976d185edf9607d7ef3472aae87f44dfd6
takusan23 <takumu1200@gmail.com>
2022-11-10 00:50:54 +0900

    通知権限を求めるように

--- app/src/main/java/io/github/takusan23/newradiosupporter/tool/PermissionCheckTool.kt
+++ app/src/main/java/io/github/takusan23/newradiosupporter/tool/PermissionCheckTool.kt
@@ -1,8 +1,9 @@
 package io.github.takusan23.newradiosupporter.tool
 
-import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.core.content.ContextCompat
 
 object PermissionCheckTool {
@@ -14,9 +15,21 @@ object PermissionCheckTool {
      * @return 権限があればtrue
      * */
     fun isGranted(context: Context): Boolean {
-        val fineLocation = ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION)
-        val readPhoneState = ContextCompat.checkSelfPermission(context, Manifest.permission.READ_PHONE_STATE)
+        val fineLocation = ContextCompat.checkSelfPermission(context, android.Manifest.permission.ACCESS_FINE_LOCATION)
+        val readPhoneState = ContextCompat.checkSelfPermission(context, android.Manifest.permission.READ_PHONE_STATE)
         return (fineLocation == PackageManager.PERMISSION_GRANTED) && (readPhoneState == PackageManager.PERMISSION_GRANTED)
+    }
+
+    /**
+     * 通知権限があるかどうか。
+     * Android 13 以降のみ対応
+     *
+     * @param context [Context]
+     * @return 権限があればtrue
+     */
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    fun isGrantedPostNotification(context: Context): Boolean {
+        return ContextCompat.checkSelfPermission(context, android.Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED
     }
 
 }
--- app/src/main/java/io/github/takusan23/newradiosupporter/ui/component/BackgroundServiceItem.kt
+++ app/src/main/java/io/github/takusan23/newradiosupporter/ui/component/BackgroundServiceItem.kt
@@ -1,28 +1,53 @@
 package io.github.takusan23.newradiosupporter.ui.component
 
+import android.os.Build
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import io.github.takusan23.newradiosupporter.R
+import io.github.takusan23.newradiosupporter.tool.PermissionCheckTool
 
-/** サービス起動項目 */
+/**
+ * サービス起動項目。
+ * 通知権限がない場合はまず求める。
+ *
+ * @param modifier [Modifier]
+ * @param onClick 押したとき。13以降は通知権限がないと呼ばれない
+ */
 @ExperimentalMaterial3Api
 @Composable
 fun BackgroundServiceItem(
     modifier: Modifier = Modifier,
     onClick: () -> Unit,
 ) {
+    val context = LocalContext.current
+    val permissionRequester = rememberLauncherForActivityResult(contract = ActivityResultContracts.RequestPermission(), onResult = { isGrant ->
+        if (isGrant) {
+            onClick()
+        }
+    })
+
     Surface(
         modifier = modifier,
         color = MaterialTheme.colorScheme.surface,
         shape = RoundedCornerShape(10.dp),
-        onClick = { onClick() }
+        onClick = {
+            // 13 以降の場合は、通知権限が必要なのでない場合はまず求める
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU && !PermissionCheckTool.isGrantedPostNotification(context)) {
+                permissionRequester.launch(android.Manifest.permission.POST_NOTIFICATIONS)
+            } else {
+                onClick()
+            }
+        }
     ) {
         CommonItem(
             icon = painterResource(id = R.drawable.ic_outline_mark_chat_unread_24),
