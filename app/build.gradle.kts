40bdbbb32e654d1f06c3c804069f1b79c6cf474f
takusan23 <takumu1200@gmail.com>
2023-01-08 03:19:42 +0900

    落ちる場合がある問題を修正した 2.1.1 を出す

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "io.github.takusan23.newradiosupporter"
         minSdk = 30
         targetSdk = 33
-        versionCode = 5
-        versionName = "2.1.0"
+        versionCode = 6
+        versionName = "2.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
