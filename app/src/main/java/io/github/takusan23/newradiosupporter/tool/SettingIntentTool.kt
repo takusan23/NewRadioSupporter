0ad72dd75d2d67484a1b53cdffca080e3b9d5278
takusan23 <takumu1200@gmail.com>
2023-02-17 02:28:58 +0900

    SIMの設定画面が出せる場合は出すように

--- app/src/main/java/io/github/takusan23/newradiosupporter/tool/SettingIntentTool.kt
+++ app/src/main/java/io/github/takusan23/newradiosupporter/tool/SettingIntentTool.kt
@@ -2,6 +2,7 @@ package io.github.takusan23.newradiosupporter.tool
 
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.provider.Settings
 import android.telephony.SubscriptionManager
 
@@ -9,15 +10,20 @@ import android.telephony.SubscriptionManager
 object SettingIntentTool {
 
     /**
-     * 端末のモバイルデータ詳細画面を開く（通信量とかSIMカード選択画面があるやつ）
+     * 端末のモバイルデータ設定画面を開く
+     * SIMカード選択画面か、モバイルデータ利用量を表示する画面のどちらか
      *
      * @param context [Context]
      */
     fun openMobileDataNetworkSetting(context: Context) {
-        // SIMカード選択
-        val defaultSubscriptionId = SubscriptionManager.getDefaultDataSubscriptionId()
-        val intent = Intent(Settings.ACTION_NETWORK_OPERATOR_SETTINGS).apply {
-            putExtra(Settings.EXTRA_SUB_ID, defaultSubscriptionId)
+        val intent = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            Intent(Settings.ACTION_MANAGE_ALL_SIM_PROFILES_SETTINGS)
+        } else {
+            // SIMカード選択
+            val defaultSubscriptionId = SubscriptionManager.getDefaultDataSubscriptionId()
+            Intent(Settings.ACTION_NETWORK_OPERATOR_SETTINGS).apply {
+                putExtra(Settings.EXTRA_SUB_ID, defaultSubscriptionId)
+            }
         }
         context.startActivity(intent)
     }
