335d8c93e9f2092f68310a60e684b7705176d1fd
takusan23 <takumu1200@gmail.com>
2023-11-21 00:09:53 +0900

    Kotlin / AGP / Jetpack Compose Compiler の更新

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,7 +37,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.7"
+        kotlinCompilerExtensionVersion = "1.5.4"
     }
     packagingOptions {
         resources {
@@ -50,7 +50,7 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("androidx.activity:activity-compose:1.8.0") // enableEdgeToEdge() 使いたい
+    implementation("androidx.activity:activity-compose:1.8.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.core:core-splashscreen:1.0.1")
