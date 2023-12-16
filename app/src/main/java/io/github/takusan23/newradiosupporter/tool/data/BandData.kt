9b2713cfc0b33c464fffc6007d8780441122a8c1
takusan23 <takumu1200@gmail.com>
2023-12-17 01:49:53 +0900

    PhysicalChannelConfig#getBand が 0 以外の場合は PhysicalChannelConfig から組み立てる

--- app/src/main/java/io/github/takusan23/newradiosupporter/tool/data/BandData.kt
+++ app/src/main/java/io/github/takusan23/newradiosupporter/tool/data/BandData.kt
@@ -1,7 +1,7 @@
 package io.github.takusan23.newradiosupporter.tool.data
 
 import android.telephony.*
-import io.github.takusan23.newradiosupporter.tool.BandDictionary
+import io.github.takusan23.newradiosupporter.tool.BandDictionaryTool
 
 /**
  * バンド情報
@@ -18,39 +18,4 @@ data class BandData(
     val earfcn: Int,
     val carrierName: String,
     val frequencyMHz: Float
-) {
-    companion object {
-        /**
-         * [CellInfo]を簡略化した[BandData]に変換する
-         *
-         * @param cellInfo [TelephonyCallback.CellInfoListener]で取れるやつ
-         * @param carrierName キャリア名。[TelephonyManager.getNetworkOperatorName]
-         * @return [BandData]。LTE/NR 以外はnullになります
-         * */
-        fun convertBandData(cellInfo: CellInfo, carrierName: String) = when (val cellIdentity = cellInfo.cellIdentity) {
-            // LTE
-            is CellIdentityLte -> {
-                val earfcn = cellIdentity.earfcn
-                BandData(
-                    isNR = false,
-                    band = BandDictionary.toLteBand(earfcn),
-                    earfcn = earfcn,
-                    carrierName = carrierName,
-                    frequencyMHz = BandDictionary.toLteFrequencyMhz(earfcn),
-                )
-            }
-            // 5G (NR)
-            is CellIdentityNr -> {
-                val nrarfcn = cellIdentity.nrarfcn
-                BandData(
-                    isNR = true,
-                    band = BandDictionary.toNrBand(nrarfcn),
-                    earfcn = nrarfcn,
-                    carrierName = carrierName,
-                    frequencyMHz = BandDictionary.toNrFrequencyMhz(nrarfcn),
-                )
-            }
-            else -> null
-        }
-    }
-}
+)
