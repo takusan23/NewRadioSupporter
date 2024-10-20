a398d55c0cd109ed759b6f3673bf70c2280e03fc
takusan23 <takumu1200@gmail.com>
2024-10-21 03:24:50 +0900

    3.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "io.github.takusan23.newradiosupporter"
         minSdk = 30
         targetSdk = 35
-        versionCode = 11
-        versionName = "3.1.0"
+        versionCode = 12
+        versionName = "3.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
