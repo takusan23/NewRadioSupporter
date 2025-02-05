6d926a6f43a11964721ae8b3ef5b5461dfc44346
takusan23 <takumu1200@gmail.com>
2025-02-06 01:19:40 +0900

    compose bom 更新となんとなく言語設定に飛べるように

--- app/src/main/java/io/github/takusan23/newradiosupporter/ui/screen/SettingScreen.kt
+++ app/src/main/java/io/github/takusan23/newradiosupporter/ui/screen/SettingScreen.kt
@@ -2,6 +2,10 @@ package io.github.takusan23.newradiosupporter.ui.screen
 
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import android.provider.Settings
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -47,6 +51,9 @@ fun SettingScreen(
     ) {
         Box(modifier = Modifier.padding(it)) {
             LazyColumn {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                    item { OpenLanguageSettingNavItem { openLanguageSetting(context) } }
+                }
                 item { OpenPrivacyPolicyNavItem { openPrivacyPolicy(context) } }
                 item { OpenSourceCodeSettingNevItem { openGitHub(context) } }
                 item { LicenseSettingNavItem { onNavigate(NavigationLinkList.SettingLicenseScreen) } }
@@ -65,6 +72,17 @@ private fun openPrivacyPolicy(context: Context) {
 private fun openGitHub(context: Context) {
     val url = "https://github.com/takusan23/NewRadioSupporter".toUri()
     context.startActivity(Intent(Intent.ACTION_VIEW, url))
+}
+
+/**
+ * 言語変更の設定画面を開く
+ * OS レベルで言語変更機能がついたのが Android 13 以上。
+ */
+@RequiresApi(Build.VERSION_CODES.TIRAMISU)
+private fun openLanguageSetting(context: Context) {
+    val intent = Intent(Settings.ACTION_APP_LOCALE_SETTINGS)
+    intent.data = Uri.fromParts("package", context.packageName, null)
+    context.startActivity(intent)
 }
 
 /**
@@ -122,6 +140,26 @@ private fun OpenSourceCodeSettingNevItem(onClick: () -> Unit) {
                 icon = painterResource(id = R.drawable.ic_outline_open_in_browser_24),
                 title = stringResource(id = R.string.open_sourcecode),
                 description = stringResource(id = R.string.open_sourcecode_description)
+            )
+        }
+    )
+}
+
+/**
+ * 言語設定画面を開く
+ *
+ * @param onClick 押したとき
+ */
+@Composable
+private fun OpenLanguageSettingNavItem(onClick: () -> Unit) {
+    Surface(
+        modifier = Modifier.fillMaxWidth(),
+        onClick = onClick,
+        content = {
+            CommonItem(
+                icon = painterResource(id = R.drawable.ic_translate_24),
+                title = stringResource(id = R.string.open_language_setting),
+                description = null
             )
         }
     )
