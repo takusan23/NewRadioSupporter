e10ad25f83a3eeeb4ecd70d93eb1d508642d52f9
takusan23 <takumu1200@gmail.com>
2025-05-03 14:11:15 +0900

    xml の方のマテリアルデザインライブラリを消す

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,7 +48,6 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.16.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
     implementation("androidx.core:core-splashscreen:1.0.1")
 
